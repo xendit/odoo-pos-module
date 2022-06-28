0ac97e39d9a7e441c373f3d5f8157340fc0c6d89
yanoandri <yano@xendit.co>
2022-06-29 00:16:57 +0700

    init settings module

--- addons/xendit/__manifest__.py
+++ addons/xendit/__manifest__.py
@@ -20,14 +20,16 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'point_of_sale'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/data.xml'
     ],
+    'qweb': ['views/xendit_receipt.xml'],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
