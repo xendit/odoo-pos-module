751e8ac18395450a80a53bb958d69185d274eccc
candra saputra <candrasaputra@live.com>
2022-06-28 09:24:12 +0700

    add init project

---
+++ addons/xendit/controllers/controllers.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# from odoo import http
+
+
+# class Xendit(http.Controller):
+#     @http.route('/xendit/xendit/', auth='public')
+#     def index(self, **kw):
+#         return "Hello, world"
+
+#     @http.route('/xendit/xendit/objects/', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('xendit.listing', {
+#             'root': '/xendit/xendit',
+#             'objects': http.request.env['xendit.xendit'].search([]),
+#         })
+
+#     @http.route('/xendit/xendit/objects/<model("xendit.xendit"):obj>/', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('xendit.object', {
+#             'object': obj
+#         })
